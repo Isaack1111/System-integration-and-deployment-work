472dcd4aa33aa379921bf93433a2b5e220d94205
Isaack1111 <isaackibrahim57@gmail.com>
2026-04-24 17:35:13 +0300

    Add files via upload

---
+++ filename.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ingiza jina lako: ");
+        String name = input.nextLine();
+
+        System.out.println("Karibu " + name);
+    }
+}
